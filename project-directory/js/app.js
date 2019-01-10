1d734b841d9bdba775c9061bfca7218f1c9fd810
abferris <abferris@gmail.com>
2019-01-09 17:32:54 -0800

    This is my update to the js and my about me page! Super excited

--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -1,23 +1,34 @@
 alert('Welcome to the quick quiz about me! My name is Aaron, and I hope that you have a lot of fun here!'); //quiz intro
-alert('Please remember, these are yes or no questions!'); //extra instructions
+var username = prompt('What is your name?')
+console.log('name',name)
+
 var resultOne;
 var resultTwo;
 var resultThree;
 var resultFour;
 var resultFive;
+var resultSix;
+var resultSeven;
+var score = 0;
+var questions = 0;
+var foods = ['PESTO', 'PESTO PASTA', 'GARLIC BREAD', 'RED WINE', 'WINE'];
 //identifying result variables
 
-var ansOne = prompt('Question One: I am the first of Three children. Is this true? Yes or no?' ).toUpperCase(); //question 1
+var ansOne = prompt('Question One: My name is Aaron Blake Ferris. Is this true? Yes or no?' ).toUpperCase(); //question 1
 console.log('Answer 1',ansOne); //log the answer
 
 
 if (ansOne === 'YES' || ansOne === 'Y'){
    resultOne = 'Correct!';
+   score++ ;
 } else if (ansOne === 'NO' || ansOne === 'N'){
      resultOne = "Wrong!";
   }else{
    resultOne = "Invalid Answer!";
   }
+  questions ++;
+  console.log('Score', `${score}/${questions} `);
+  
   console.log('Result 1', resultOne);
  alert(`${resultOne} Yes it is.`); //STRING INTERPOLATION help from Billy who got it from code academy
 
@@ -29,9 +40,12 @@ if (ansTwo === 'YES' || ansTwo === 'Y'){
    resultTwo = 'Wrong!';
 } else if (ansTwo === 'NO' || ansTwo === 'N'){
      resultTwo = "Correct!";
+     score++;
   }else{
    resultTwo = "Invalid Answer!";
   }
+  questions ++;
+  console.log('Score', `${score}/${questions} `);
   console.log('Result 2', resultTwo);
  alert(`${resultTwo} No. I am the youngest of three.`);//STRING INTERPOLATION help from Billy who got it from code academy
 
@@ -40,11 +54,14 @@ console.log('Answer 3',ansThree); //log the answer
 
 if (ansThree === 'YES' || ansThree === 'Y'){
    resultThree = 'Correct!';
+   score ++;
 } else if (ansThree === 'NO' || ansThree === 'N'){
      resultThree = "Wrong!";
   }else{
    resultThree = "Invalid Answer!";
   }
+  questions ++;
+  console.log('Score', `${score}/${questions} `);
   console.log('Result 3', resultThree);
  alert(`${resultThree} Yes. Thank goodness they decided otherwise, which kept me from much childhood teasing. Now Key and Peel have opened that can of worms for my name`); //STRING INTERPOLATION help from Billy who got it from code academy
 
@@ -55,9 +72,12 @@ if (ansFour === 'YES' || ansFour === 'Y'){
    resultFour = 'Wrong!';
 } else if (ansFour === 'NO' || ansFour === 'N'){
      resultFour = "Correct!";
+     score++;
   }else{
    resultFour = "Invalid Answer!";
   }
+  questions ++;
+  console.log('Score', `${score}/${questions} `);
   console.log('Result 4', resultFour);
  alert(`${resultFour} No. Unfortunatly life gets in the way. Codefellows is busy enough that I do not have the time to commit to something like that during the school day. You should know that!`); //STRING INTERPOLATION help from Billy who got it from code academy
 
@@ -68,8 +88,56 @@ if (ansFive === 'YES' || ansFive === 'Y'){
    resultFive = 'Wrong!';
 } else if (ansFive === 'NO' || ansFive === 'N'){
      resultFive = "Correct!";
+     score ++;
   }else{
    resultFive = "Invalid Answer!";
   }
+  questions ++;
+  console.log('Score', `${score}/${questions} `);
   console.log('Result 5', resultFive);
  alert(`${resultFive} No. If you know anything about me, I am a glutton for punishment! I enjoy playing magic-users, as knowing and managing your spells is often more complex and difficult. However dumb characters can be played in VERY fun ways.`); //STRING INTERPOLATION help from Billy who got it from code academy
+
+for(var i=4; i>0; i--) {
+   console.log('attempts', i)
+
+   var ansSix = prompt(`Question Six: How old am I? You currently have ${i} attempts left.`);//question six
+   console.log('Answer 6',ansSix);
+   ansSix = parseInt(ansSix);
+   if (ansSix === 27){
+      alert('Correct!');
+      score ++;
+      i=0;
+      console.log('Question 6', 'Correct');
+   } else
+   if (ansSix<27){
+      alert('Too Low Joe!');
+      console.log('Question 6', 'Wrong');
+
+   }else
+   if (ansSix>27){
+      alert('Too high guy!');
+      console.log('Question 6', 'Wrong');
+   } else{
+      alert('Invalid answer fool!');
+      console.log('Question 6', 'Wrong');
+   }
+}
+questions ++;
+
+for (var j=6; j>0; j--){
+   console.log('attempts', j)
+   var ansSeven = prompt(`What are one of the peices of my favorite meal my parents make? You have ${j} attempts left.`).toUpperCase();
+
+   console.log ('Answer 7', ansSeven)
+
+   for (var k=0; k<foods.length; k++){
+      console.log('test',foods[k])
+if ( ansSeven === foods[k] ) {
+   
+   alert('Correct!');
+   score ++;
+   j=0;
+   }
+}
+}
+ alert(`THE RESULTS ARE IN! ${username} you scored ${score}/${questions}`);
