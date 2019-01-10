472513cc697ab8bfe261ac58070b915f7e2df18b
abferris <abferris@gmail.com>
2019-01-10 12:25:26 -0800

    This is my life

--- project-directory/README.md
+++ project-directory/README.md
@@ -1,2 +1,4 @@
 # CF201-Lab2
 Lab 2 of course 201 'About Me'
+
+String Interpolation from codeacademy.com. Routed to tool by Billy Bunn
--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -14,9 +14,9 @@ var questions = 0;
 var foods = ['PESTO', 'PESTO PASTA', 'GARLIC BREAD', 'RED WINE', 'WINE'];
 //identifying result variables
 
-var ansOne = prompt('Question One: My name is Aaron Blake Ferris. Is this true? Yes or no?' ).toUpperCase(); //question 1
+//question 1
+var ansOne = prompt('Question One: My name is Aaron Blake Ferris. Is this true? Yes or no?' ).toUpperCase(); 
 console.log('Answer 1',ansOne); //log the answer
-
 
 if (ansOne === 'YES' || ansOne === 'Y'){
    resultOne = 'Correct!';
@@ -27,13 +27,14 @@ if (ansOne === 'YES' || ansOne === 'Y'){
    resultOne = "Invalid Answer!";
   }
   questions ++;
-  console.log('Score', `${score}/${questions} `);
   
+  console.log('Score', `${score}/${questions} `);
   console.log('Result 1', resultOne);
+
  alert(`${resultOne} Yes it is.`); //STRING INTERPOLATION help from Billy who got it from code academy
 
-
- var ansTwo = prompt('Question Two: I am the oldest of three children. Is this true? Yes or no?').toUpperCase(); //question 2
+//question 2
+ var ansTwo = prompt('Question Two: I am the oldest of three children. Is this true? Yes or no?').toUpperCase(); 
 console.log('Answer 2',ansTwo); //log the answer
 
 if (ansTwo === 'YES' || ansTwo === 'Y'){
@@ -45,11 +46,14 @@ if (ansTwo === 'YES' || ansTwo === 'Y'){
    resultTwo = "Invalid Answer!";
   }
   questions ++;
+  
   console.log('Score', `${score}/${questions} `);
   console.log('Result 2', resultTwo);
- alert(`${resultTwo} No. I am the youngest of three.`);//STRING INTERPOLATION help from Billy who got it from code academy
+ 
+  alert(`${resultTwo} No. I am the youngest of three.`);//STRING INTERPOLATION help from Billy who got it from code academy
 
- var ansThree = prompt('Question Three: My two older brothers are named Drew and Reed. As my father and grandmother are both teachers, I was almost named Wright. Is this true? Yes or no?').toUpperCase(); //question 3
+ //question 3
+ var ansThree = prompt('Question Three: My two older brothers are named Drew and Reed. As my father and grandmother are both teachers, I was almost named Wright. Is this true? Yes or no?').toUpperCase(); 
 console.log('Answer 3',ansThree); //log the answer
 
 if (ansThree === 'YES' || ansThree === 'Y'){
@@ -61,11 +65,14 @@ if (ansThree === 'YES' || ansThree === 'Y'){
    resultThree = "Invalid Answer!";
   }
   questions ++;
+ 
   console.log('Score', `${score}/${questions} `);
   console.log('Result 3', resultThree);
- alert(`${resultThree} Yes. Thank goodness they decided otherwise, which kept me from much childhood teasing. Now Key and Peel have opened that can of worms for my name`); //STRING INTERPOLATION help from Billy who got it from code academy
+ 
+  alert(`${resultThree} Yes. Thank goodness they decided otherwise, which kept me from much childhood teasing. Now Key and Peel have opened that can of worms for my name`); //STRING INTERPOLATION help from Billy who got it from code academy
 
- var ansFour = prompt('Question Four: I have played ultimate at a competitive level, known as club. I also currently enjoy giving back to the ultimate community by coaching teams my at alma mater Eckstein Middle School. Is this true? Yes or no?').toUpperCase(); //question 4
+ //question 4
+ var ansFour = prompt('Question Four: I have played ultimate at a competitive level, known as club. I also currently enjoy giving back to the ultimate community by coaching teams my at alma mater Eckstein Middle School. Is this true? Yes or no?').toUpperCase(); 
 console.log('Answer 4',ansFour); //log the answer
 
 if (ansFour === 'YES' || ansFour === 'Y'){
@@ -77,11 +84,14 @@ if (ansFour === 'YES' || ansFour === 'Y'){
    resultFour = "Invalid Answer!";
   }
   questions ++;
+
   console.log('Score', `${score}/${questions} `);
   console.log('Result 4', resultFour);
+
  alert(`${resultFour} No. Unfortunatly life gets in the way. Codefellows is busy enough that I do not have the time to commit to something like that during the school day. You should know that!`); //STRING INTERPOLATION help from Billy who got it from code academy
 
- var ansFive = prompt('Question Five: I play Dungeons and Dragons! I prefer to play hack and slash fighting characters to make things as simple as possible. Is this true? Yes or no?').toUpperCase(); //question 5
+ //question 5
+ var ansFive = prompt('Question Five: I play Dungeons and Dragons! I prefer to play hack and slash fighting characters to make things as simple as possible. Is this true? Yes or no?').toUpperCase(); 
 console.log('Answer 5',ansFive); //log the answer
 
 if (ansFive === 'YES' || ansFive === 'Y'){
@@ -93,9 +103,11 @@ if (ansFive === 'YES' || ansFive === 'Y'){
    resultFive = "Invalid Answer!";
   }
   questions ++;
+
   console.log('Score', `${score}/${questions} `);
   console.log('Result 5', resultFive);
- alert(`${resultFive} No. If you know anything about me, I am a glutton for punishment! I enjoy playing magic-users, as knowing and managing your spells is often more complex and difficult. However dumb characters can be played in VERY fun ways.`); //STRING INTERPOLATION help from Billy who got it from code academy
+
+  alert(`${resultFive} No. If you know anything about me, I am a glutton for punishment! I enjoy playing magic-users, as knowing and managing your spells is often more complex and difficult. However dumb characters can be played in VERY fun ways.`); //STRING INTERPOLATION help from Billy who got it from code academy
 
 for(var i=4; i>0; i--) { //loop for attempts
    console.log('attempts', i); // log tries
@@ -103,6 +115,7 @@ for(var i=4; i>0; i--) { //loop for attempts
    var ansSix = prompt(`Question Six: How old am I? You currently have ${i} attempts left.`);//question six
    console.log('Answer 6',ansSix); //log answer
    ansSix = parseInt(ansSix);
+
    if (ansSix === 27){ //if correct
       alert('Correct!');
       score ++;
@@ -127,17 +140,16 @@ questions ++;
 for (var j=6; j>0; j--){ // counter
    console.log('attempts', j); //log tries
    var ansSeven = prompt(`What are one of the peices of my favorite meal my parents make? You have ${j} attempts left.`).toUpperCase(); //seventh question
-
    console.log ('Answer 7', ansSeven); // log answer seven
 
    for (var k=0; k<foods.length; k++){// loop for running answer against arrays
       console.log('test',foods[k]);// log which foods it looked at
       if ( ansSeven === foods[k] ) {//is check against part of array
-   
    alert('Correct!');// good job
    score ++; 
    j=0;// end first for loop
       }
    }
 }
+
  alert(`THE RESULTS ARE IN! ${username} you scored ${score}/${questions}`);// result
